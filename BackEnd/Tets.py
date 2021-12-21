94e10690141472837184a3266b7aefb739fbfc7c
Hossein <hossein.dehghanipour1998@gmail.com>
2021-12-21 01:28:18 +0100

    Tested on a more complicated test case

--- BackEnd/Tets.py
+++ BackEnd/Tets.py
@@ -3,12 +3,12 @@ import Calculator as Cal
 
 
 
-expr = "2 ^ 1 ^ 3"
+expr = "3 / 1 + 2 * 2 / 5 - 9 / 4 * 2.5"
 #expr = "142 / 6 / 7 / 2 + 1"
 cal =  Cal.Calculator(expr, -1)
 result = cal.calculate()
 print(result)
 
-square_number = eval('2**1**3')
-print(square_number)
+square_number = eval("3 / 1 + 2 * 2 / 5 - 9 / 4 * 2.5")
+print(square_number == result)
 
