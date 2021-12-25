1854ca6cd58ff4faf6890dd7284709dee061a610
Hossein <hossein.dehghanipour1998@gmail.com>
2021-12-25 20:55:00 +0100

    Added Functions: sin, cos, sinh, cosh, tan, ctg, ceil, floor, sqrt, log, exp

--- BackEnd/Calculator.py
+++ BackEnd/Calculator.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 class Calculator :
 
@@ -6,13 +7,14 @@ class Calculator :
         self.length = len(expression)
         self.operatorStack = []
         self.operandStack = []
-        self.expr = expression.split()
+        self.expr = list(expression.split())
         self.operatorsList = {'+' , '-' , '/' , '*' , '^'}
         self.operatorPrecedences = { "+" : 1 , "-": 1 , "/" : 3 , "*" : 3 , "^" : 4 }
         self.x = inputX
+        self.isCallingFunction = (True if ( str(self.expr[0]).upper() == "FUNCTION") else False)
         
         ###### Used for debugging (Start)######
-        #print("Expression : {%s} \n Len : {%d} \n input : {%d} \n " %(self.expr , len(self.expr) , self.x))
+        print(f"Expression : {self.expr} \n Len : {len(self.expr)} \n input : { self.x} \n Function? {self.isCallingFunction}\n ")
         ###### Used for debugging (End)######
 
     def operandAction(self,operator, input1 , input2):
@@ -65,9 +67,28 @@ class Calculator :
         _float_regexp = re.compile(r"^[-+]?(?:\b[0-9]+(?:\.[0-9]*)?|\.[0-9]+\b)(?:[eE][-+]?[0-9]+\b)?$").match
         return True if _float_regexp(string) else False
     
+    def calculateFunction(self, functionName, operand):
+
+        switchCase = {
+            "tan": math.tan(operand),
+            "ctg": 1/math.tan(operand),
+            "sin": math.sin(operand),
+            "cos": math.cos(operand),
+            "ceil": math.ceil(operand),
+            "floor": math.floor(operand),
+            "sqrt": math.sqrt(operand),
+            "log": math.log(operand),
+            "exp": math.exp(operand),
+            "cosh":math.cosh(operand),
+            "sinh":math.sinh(operand)
+            }
+        
+        
+        result =  switchCase.get(functionName, "Not Found")
+        return result
+        
     
-    
-    def calculate(self):
+    def calculateOperator(self):
         '''
         Parameters: Gets nothing because it uses the expression variable from the class.
 
@@ -120,12 +141,12 @@ class Calculator :
                     self.operandStack.append(result)
                     stackTop = self.operatorStack.pop()
             
-            # If Character == Variable
+            # If Character == Varibale X 
             elif ( character.isalpha() == True ):
                 self.operandStack.append(self.x)
                 
                 
-            # If Character == Number
+            # If Character == Operand (Number)
             elif ( (character.isdigit() == True) or (self.is_float(character)) ):
                 self.operandStack.append(float(character))
        
@@ -138,7 +159,30 @@ class Calculator :
             self.operandStack.append(result)
             
             ###### Used for debugging (Start)######
-            print("Operation: " + str(operator) + " Operands: " + str(a) + " " + str(b))
+            #print("Operation: " + str(operator) + " Operands: " + str(a) + " " + str(b))
             ###### Used for debugging (End)######
 
         return self.operandStack.pop()
+
+
+    def calculate(self):
+        result = None
+        
+        if(self.isCallingFunction):          
+            functionName =  str(self.expr[1]).lower()         
+            operand =  int(self.expr[2])
+            result = self.calculateFunction(functionName, operand)
+            
+            ###### Used for debugging (Start)######
+            print(f"Expr: {self.expr}")
+            print(f"Function Name: ({str(self.expr[1]).lower()})")
+            print(f"Function Name: {functionName} | Operand: {operand}")
+            ###### Used for debugging (End)######
+            
+        else:
+            result = self.calculateOperator()
+        
+        return result
+            
+            
+        
--- BackEnd/Tets.py
+++ BackEnd/Tets.py
@@ -3,12 +3,91 @@ import Calculator as Cal
 
 
 
-expr = "3 / 1 + 2 * 2 / 5 - 9 / 4 * 2.5"
-#expr = "142 / 6 / 7 / 2 + 1"
+'''
+Functions: 
+    sin
+    cos
+    sinh
+    cosh
+    tan
+    ctg
+    ceil
+    floor
+    sqrt
+    log
+    exp
+'''
+
+# How to Pass it?
+expr = "function sin 90"
+expr = "31 / 1 + 2 * 2 / 5 - 9 / 4 * 2.5"
+
 cal =  Cal.Calculator(expr, -1)
 result = cal.calculate()
+
+
+
+
+
+
+#expr = "142 / 6 / 7 / 2 + 1"
+
 print(result)
 
-square_number = eval("3 / 1 + 2 * 2 / 5 - 9 / 4 * 2.5")
+square_number = eval("31 / 1 + 2 * 2 / 5 - 9 / 4 * 2.5")
 print(square_number == result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
