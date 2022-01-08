b341bda5c6eb634a76e1d23003da69c9e2af9803
Hossein <hossein.dehghanipour1998@gmail.com>
2022-01-08 01:32:35 +0100

    Tested on more sophisticated calculations

--- BackEnd/Tets.py
+++ BackEnd/Tets.py
@@ -18,11 +18,8 @@ Functions:
     exp
 '''
 
-# How to Pass it?
-#expr = "1 + 2 * 3 * cos ( sin ( 89 + 1 ) ) * tan ( 0 ) - 1"
-expr = "2 ^ 2 ^ 3"
- 
-#expr = "31 / 1 + 2 * 2 / 5 - 9 / 4 * 2.5"
+
+expr = "32 / 3.2 + 2 * 2 / 5 - 9 / 4 * 2.5"
 
 cal =  Cal.Calculator(expr, -1)
 result = cal.calculate()
@@ -35,10 +32,9 @@ result = cal.calculate()
 #expr = "142 / 6 / 7 / 2 + 1"
 
 print(result)
-import re
-p = r"^"
-re.sub(p,"**",expr)
-square_number = eval(expr)
+
+square_number = eval("32/3.2+2*2/5-9/4*2.5")
+print(square_number)
 print(square_number == result)
 
 
